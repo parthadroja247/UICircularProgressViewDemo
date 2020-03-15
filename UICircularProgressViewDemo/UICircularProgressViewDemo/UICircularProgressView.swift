26da9146d20df4267292d7565ece799ee9b293f2
Parth Adroja <parthadroja247@gmail.com>
2020-03-15 13:09:01 +0530

    Added demo project
    Signed-off-by: Parth Adroja <parthadroja247@gmail.com>

---
+++ UICircularProgressViewDemo/UICircularProgressViewDemo/UICircularProgressView.swift
@@ -0,0 +1,9 @@
+//
+//  UICircularProgressView.swift
+//  UICircularProgressViewDemo
+//
+//  Created by Parth Adroja on 15/03/20.
+//  Copyright © 2020 Parth Adroja. All rights reserved.
+//
+
+import Foundation
---
+++ UICircularProgressViewDemo/UICircularProgressViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICircularProgressViewDemo
+//
+//  Created by Parth Adroja on 15/03/20.
+//  Copyright © 2020 Parth Adroja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
