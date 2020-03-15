019001d08e0b671e31956be309ddf99f5d31b86b
Parth Adroja <parthadroja247@gmail.com>
2020-03-15 13:09:42 +0530

    Updated
    Signed-off-by: Parth Adroja <parthadroja247@gmail.com>

--- UICircularProgressViewDemo/UICircularProgressViewDemo/UICircularProgressView.swift
+++ UICircularProgressViewDemo/UICircularProgressViewDemo/UICircularProgressView.swift
@@ -6,4 +6,91 @@
 //  Copyright © 2020 Parth Adroja. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class UICircularProgressView: UIView {
+    
+    var backgroundCircleColor: UIColor = .black {
+        didSet {
+            backgroundCircle.strokeColor = backgroundCircleColor.cgColor
+        }
+    }
+    var fillColor: UIColor = .white {
+        didSet {
+            progressCircle.strokeColor = fillColor.cgColor
+        }
+    }
+    var lineWidth: CGFloat = 4 {
+        didSet {
+            backgroundCircle.lineWidth = lineWidth
+            progressCircle.lineWidth = lineWidth
+        }
+    }
+    var defaultProgress: CGFloat = 0 {
+        didSet {
+            updateProgress(progress: defaultProgress)
+        }
+    }
+    
+    private var backgroundCircle: CAShapeLayer!
+    private var progressCircle: CAShapeLayer!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configureView()
+    }
+    
+    private func configureView() {
+        drawBackgroundCircle()
+        drawProgressCircle()
+    }
+    
+    private func drawBackgroundCircle() {
+        backgroundCircle = CAShapeLayer()
+        let centerPoint = CGPoint(x: self.bounds.width / 2, y: self.bounds.width / 2)
+        let circleRadius: CGFloat = self.bounds.width / 2
+        let circlePath = UIBezierPath(arcCenter: centerPoint,
+                                      radius: circleRadius,
+                                      startAngle: CGFloat(-0.5 * .pi),
+                                      endAngle: CGFloat(1.5 * .pi),
+                                      clockwise: true)
+        backgroundCircle.path = circlePath.cgPath
+        backgroundCircle.strokeColor = UIColor.green.cgColor
+        backgroundCircle.fillColor = UIColor.clear.cgColor
+        backgroundCircle.lineWidth = 4
+        backgroundCircle.lineCap = .round
+        backgroundCircle.lineJoin = .round
+        backgroundCircle.strokeStart = 0
+        backgroundCircle.strokeEnd = 1.0
+        self.layer.addSublayer(backgroundCircle)
+    }
+    
+    private func drawProgressCircle() {
+        progressCircle = CAShapeLayer()
+        let centerPoint = CGPoint(x: self.bounds.width / 2, y: self.bounds.width / 2)
+        let circleRadius: CGFloat = self.bounds.width / 2
+        let circlePath = UIBezierPath(arcCenter: centerPoint,
+                                      radius: circleRadius,
+                                      startAngle: CGFloat(-0.5 * .pi),
+                                      endAngle: CGFloat(1.5 * .pi),
+                                      clockwise: true)
+        progressCircle.path = circlePath.cgPath
+        progressCircle.strokeColor = UIColor.red.cgColor
+        progressCircle.fillColor = UIColor.clear.cgColor
+        progressCircle.lineWidth = 4
+        progressCircle.lineCap = .round
+        progressCircle.lineJoin = .round
+        progressCircle.strokeStart = 0
+        progressCircle.strokeEnd = 0.0
+        self.layer.addSublayer(progressCircle)
+    }
+    
+    func updateProgress(progress: CGFloat) {
+        progressCircle.strokeEnd = CGFloat(progress / 100)
+    }
+}
--- UICircularProgressViewDemo/UICircularProgressViewDemo/ViewController.swift
+++ UICircularProgressViewDemo/UICircularProgressViewDemo/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var eg1ProgressView: UICircularProgressView!
+    @IBOutlet weak var eg2ProgressView: UICircularProgressView!
+    @IBOutlet weak var otpLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureView()
     }
-
-
+    
+    private func configureView() {
+        configureEg1ProgressView()
+        configureEg2ProgressView()
+    }
+    
+    private func configureEg1ProgressView() {
+        var currentProgress: CGFloat = 0.0
+        eg1ProgressView.defaultProgress = currentProgress
+        Timer.scheduledTimer(withTimeInterval: 1,
+                             repeats: true,
+                             block: { timer in
+                                currentProgress += 2
+                                self.eg1ProgressView.updateProgress(progress: currentProgress)
+                                if currentProgress >= 100 {
+                                    timer.invalidate()
+                                }
+        })
+    }
+    
+    private func configureEg2ProgressView() {
+        var currentProgress: CGFloat = 100.0
+        eg2ProgressView.defaultProgress = currentProgress
+        otpLabel.text = [1,2,3,4,5,6].shuffled().map({ String($0) }).joined()
+        Timer.scheduledTimer(withTimeInterval: 1,
+                             repeats: true,
+                             block: { timer in
+                                currentProgress -= 10
+                                self.eg2ProgressView.updateProgress(progress: currentProgress)
+                                if currentProgress <= 0 {
+                                    self.otpLabel.text = [1,2,3,4,5,6].shuffled().map({ String($0) }).joined()
+                                    currentProgress = 100
+                                }
+        })
+    }
 }
-
